5d3a7e036f1b574421c079a654a5d1720077e56b
Justin Wilkey <justin.wilkey@tentkeep.com>
2018-09-01 16:30:18 -0400

    Updates

---
+++ precache-manifest.23fa6cf9d57b3a1d7cbbe44a350f174c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb103ea8e884f71c1100c9e5fe696291",
+    "url": "/img/collapse.fb103ea8.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b5c7f0351a3d888baad1e1cee0537df8",
+    "url": "/img/play.b5c7f035.svg"
+  },
+  {
+    "revision": "09cbff12951f4dc39a9e",
+    "url": "/js/chunk-vendors.7988de65.js"
+  },
+  {
+    "revision": "9a503b87a5d6cf873633",
+    "url": "/js/app.93b2128c.js"
+  },
+  {
+    "revision": "ed91954bc28d49e4b0bf81cf070e8fb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a36aa23c40f1932a32e2e599fefac4c",
+    "url": "/img/wavebar_padded.6a36aa23.png"
+  },
+  {
+    "revision": "c0eac97d39cced0bafa045250630d078",
+    "url": "/img/pause.c0eac97d.svg"
+  },
+  {
+    "revision": "9512482db10c07a61ace5b9485c03b81",
+    "url": "/img/curves.9512482d.jpg"
+  },
+  {
+    "revision": "1cdc8fce9609d07f0e9d8d0bc4b61f8f",
+    "url": "/img/btn_google_light_normal_ios.1cdc8fce.svg"
+  },
+  {
+    "revision": "7de99c806ccebdc2a4089b7bf493b567",
+    "url": "/fonts/BookIcons.7de99c80.ttf"
+  },
+  {
+    "revision": "68c1c1e5c474b976ed913924cea6140d",
+    "url": "/fonts/BookIcons.68c1c1e5.woff"
+  },
+  {
+    "revision": "bb3ad89e297b3ac86860d8b43c077d3a",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "9a503b87a5d6cf873633",
+    "url": "/css/app.68687d0c.css"
+  }
+];
